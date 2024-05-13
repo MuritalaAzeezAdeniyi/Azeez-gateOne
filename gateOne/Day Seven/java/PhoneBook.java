f9fbbd793bf6a47968b9210053b507f296d5bfad
MuritalaAzeezAdeniyi <azeezmuritalaadeniyi@gmail.com>
2024-05-13 15:26:28 -0700

    add new file

--- gateOne/Day Seven/java/PhoneBook.java
+++ gateOne/Day Seven/java/PhoneBook.java
@@ -1,17 +1,38 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class PhoneBook{
-	public static void main(String  args){
+        public static void main(String  args){
+	ArrayList<String> name = new ArrayList<String>();
+	ArrayList<Double>number= new ArrayList<Double>();
 	Scanner input = new Scanner(System.in);
 		
-		System.out.print("Enter your name");
-		String name = input.nextLine();
-		
-		System.out.Print("Enter your contact");
-		int contact = input.nextInt();
-public void printprompt(){
-	String prompt ="""1. Add contact \n" + "2.Remove contact\n"
-+"3. find contact by phone number\n" +" 4. find contact by first number\n" + " 5.find contact by last name\n" +" 6.Edit contact """;
+ String prompt = """
+1 Add contact; 
+2 Remove contact;
+3 Find contact by phone number;
+4 Find contact by first name;
+5 Find contact by last name;
+6 Edit contact;
+""";
+ System.out.print(prompt)
+int phoneBook = input.nextInt
+Switch(phoneBook){
+1-> Add contact;
+2-> Remove contact;
+3-> Find contact by phone number;
+4-> Find contact by first name;
+5-> Find contact by last name;
+6-> Edit contact; 		
+}
+ 
+do{
+System.out.print("Enter name");
+String name = input.nextline();
 
+System.out.print("Enter number");
+int number = input.nextInt();
+
+}while (name.qualsIgnoreCase("yes"));
 
 
 
