c39c4d1829fbcc8f6fd96a6d4165dfb218d425c3
MuritalaAzeezAdeniyi <azeezmuritalaadeniyi@gmail.com>
2024-05-15 22:58:17 -0700

    add new file

---
+++ gateOne/Day Seven/java/MenstrualCalculator.java
@@ -0,0 +1,50 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+public class MenstrualCalculator{
+	
+	public static void min(String[]  args){ 
+
+		Scanner input = new Scanner(System.in);
+
+System.out.println("Welcome to Abdulazeez Menstrual Cycle Calculator! We're happy to help you track your cycle and stay informed about your body, Let's get started and help you better understand your cycle.");
+
+
+
+System.out.print("Enter The Date of your last Menstrual flow (DD-MM-YYYY): ");
+String dateOfLastFlow = input.nextLine();
+
+System.out.print("Enter The Average Length of your Cycle In Day: ");
+int lengthOfCycle = input.nextInt();
+
+DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-mm-yyyy");
+
+LocalDate dataOfLastFlow = LocalDate.parse(dateOfLastFlow,formatter);
+
+LocalDate nextMenstrual = dateOfLastFlow.plusDays(lengthOfCycle);
+
+System.out.print("Your Next Menstrual will starts On: " + nextMenstrual);
+
+LocalDate ovulationDate = nextMenstrual.minusDays(14);
+
+System.out.print("Your ovulation Date Is: " + ovulationDate);
+
+LocalDate fertilePriodSter = ovulationDate.minusDays(7);
+
+LocalDate fertilePriodEnd = ovulationDate.plusDays(2);
+
+System.out.print("Your fertile period is Between: "+ ovulationDate.minusDays(7)+" - " + ovulationDate.plusDays(2));
+
+LocalDate safePeriodStart = ovulationDate.plusDays(5);
+
+LocalDate SafePeriodEnd = nextMenstrual.minusDays(5);
+
+System.out.println("Your safe period is Between " + ovulationDate.plusDays(5)+ " -" + nextMenstrual.minusDays(5) );
+
+
+
+
+
+
+}
+}
